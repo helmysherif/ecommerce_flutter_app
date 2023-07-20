e8b03a8710eab9246e0f438363779becb8b3864a
Sherif <Sherif.Helmy@qeema.net>
2023-07-20 14:50:45 -0700

    init the app

---
+++ lib/app.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class EcommerceApp extends StatelessWidget {
+  const EcommerceApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Flutter Demo',
+    );
+  }
+}
